0a8d6e43936d81a6b59b9b4fb344448a3b49e96f
Marat Dospolov <marat.dospolov@gmail.com>
2019-01-19 21:44:35 +0200

    removed test param

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import { addTypenameToDocument } from 'apollo-utilities';
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
-const generatePath = () => "organizations?token=d2a458be144dbcb2e8ed01d0b95c2a274dbe70873f8ceaa1a6180ddaf9487495&key=e9d4b0061c2ac9a0529240b09d88521c"
+const addToken = path => `${path}?token=d2a458be144dbcb2e8ed01d0b95c2a274dbe70873f8ceaa1a6180ddaf9487495&key=e9d4b0061c2ac9a0529240b09d88521c`
 
 const GetTeams = gql`
-  query ($test: String!) {
-    teams @rest(type: "Teams", path: $test
+  query ($path: String!) {
+    teams @rest(type: "Teams", path: $path
     ) {
       displayName
       id
@@ -16,7 +15,7 @@ const GetTeams = gql`
 `;
 
 const Teams = () => (
-  <Query query={GetTeams} variables={{ test: generatePath() }}>
+  <Query query={GetTeams} variables={{ path: addToken('organizations') }}>
     {({ loading, error, data }) => {
       if (loading) return null
       if (error) return `Error!: ${error}`
