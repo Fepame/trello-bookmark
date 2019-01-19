baf18f6d0ceaa8f44045585d56d10b39eedddf73
Marat Dospolov <marat.dospolov@gmail.com>
2019-01-19 21:34:02 +0200

    passing path

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import { addTypenameToDocument } from 'apollo-utilities';
 
-const Query = gql`
-  query {
-    teams @rest(type: "Teams", path: 
-        "organizations?token=d2a458be144dbcb2e8ed01d0b95c2a274dbe70873f8ceaa1a6180ddaf9487495&key=e9d4b0061c2ac9a0529240b09d88521c",
+const generatePath = () => "organizations?token=d2a458be144dbcb2e8ed01d0b95c2a274dbe70873f8ceaa1a6180ddaf9487495&key=e9d4b0061c2ac9a0529240b09d88521c"
+
+const GetTeams = gql`
+  query ($test: String!) {
+    teams @rest(type: "Teams", path: $test
     ) {
       displayName
       id
@@ -14,36 +15,17 @@ const Query = gql`
   }
 `;
 
-class Teams extends Component {
-  render() {
-    const { loading, error, teams } = this.props;
-    if (loading) {
-      return <h4>Loading...</h4>;
-    }
-    if (error) {
-      return <h4>{error.message}</h4>;
-    }
-    return <ul>
-      {teams.map(team => <li key={team.id}>{team.displayName}</li>)}
-    </ul>;
-  }
-}
-export default graphql(Query, {
-  props: ({ data }) => {
-    if (data.loading) {
-      return {
-        loading: data.loading,
-      };
-    }
+const Teams = () => (
+  <Query query={GetTeams} variables={{ test: generatePath() }}>
+    {({ loading, error, data }) => {
+      if (loading) return null
+      if (error) return `Error!: ${error}`
 
-    if (data.error) {
-      return {
-        error: data.error,
-      };
-    }
-    return {
-      teams: data.teams,
-      loading: false,
-    };
-  },
-})(Teams);
+      return <ul>
+        {data.teams.map(team => <li key={team.id}>{team.displayName}</li>)}
+      </ul>
+    }}
+  </Query>
+)
+
+export default Teams
