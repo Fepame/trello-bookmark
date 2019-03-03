bb3c47c324eb113f6459695dcc688e3bcde50b3b
Marat Dospolov <marat.dospolov@gmail.com>
2019-03-03 18:34:11 +0200

    title cache

--- src/components/pages/main/fields/Title.js
+++ src/components/pages/main/fields/Title.js
@@ -1,33 +1,42 @@
 import React from 'react'
 import { Input } from 'antd'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 import { getTabInfo } from './../../../../services/utils'
 const { TextArea } = Input
 
-const Title = ({ setCardField, card })  => {
-  getTabInfo(tab => setCardField({
-    variables: {
-      fieldName: "title",
-      fieldValue: tab.title,
-      __typename: "Card"
+// getTabInfo(tab => setCardField({
+//   variables: {
+//     fieldName: "title",
+//     fieldValue: tab.title,
+//     __typename: "Card"
+//   }
+// }))
+export default () => (
+  <Query query={gql`
+    {
+      card {
+        title
+      }
     }
-  }))
+  `}>
+    {({ data: {card: { title }}, client }) => (
+      <TextArea 
+        placeholder="Card title" 
+        autosize={{ minRows: 2, maxRows: 2 }}
+        value={title}
+        onChange={e => {
+          client.writeData({
+            data: {
+              card: {
+                title: e.target.value,
+                __typename: "Card"
+              }
+            }
+          })
+        }}
+      />
+    )}
+  </Query>
+)
 
-  return (
-    <TextArea 
-      placeholder="Card title" 
-      autosize={{ minRows: 2, maxRows: 2 }}
-      value={card.title}
-      onChange={e => {
-        setCardField({
-          variables: {
-            fieldName: "title",
-            fieldValue: e.target.value,
-            __typename: "Card"
-          }
-        })
-      }}
-    />
-  )
-}
-
-export default Title
