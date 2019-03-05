e8dab5dc773541cf02e5f4747d8fd2c9422ccbd2
Marat Dospolov <marat.dospolov@gmail.com>
2019-03-05 17:16:21 +0200

    Revert "update manifest version based on package json"
    This reverts commit 23a8f24a82096a262b9b2f876fa34fd4da5a3dbb.

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,5 @@
 const CracoAntDesignPlugin = require("craco-antd");
-const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 const webpack = require("webpack")
-const pjson = require('./package.json');
 
 module.exports = {
   plugins: [{ plugin: CracoAntDesignPlugin }],
@@ -9,15 +7,7 @@ module.exports = {
     plugins: [
       new webpack.DefinePlugin({
         '__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'
-      }),
-      new ReplaceInFileWebpackPlugin([{
-        dir: './public',
-        files: ['manifest.json'],
-        rules: [{
-            search: /"version": ".*",/i,
-            replace: `"version": "${pjson.version}",`
-        }]
-      }])
+      })
     ]
   }
 };
